2ba171b41d297581526b3585875ab8d0d66ea9e9
HeldyisAE <heaguero@estudiantec.cr>
2025-11-08 13:44:42 -0600

    Reorganiza la estructura de archivos con el DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="app">
-      <img src="/Logo_ManiaTico.png" alt="Logo ManiaTico" className="logo" />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="home">
+      <img src="/Logo_ManiaTico.png" alt="Logo ManiaTico" className="logo" />
+    </div>
+  )
+}
+
+export default Home
