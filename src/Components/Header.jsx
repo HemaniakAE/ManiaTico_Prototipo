01dca1ca887a0709a973b9d676c66e73a54073c8
HeldyisAE <heaguero@estudiantec.cr>
2025-11-08 20:42:07 -0600

    Agrega estilos para los elementos del header y el header en sí

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import './Header.css'
+import { Link } from "react-router-dom";
+import "./Header.css";
 import RefreshButton from "./RefreshButton";
+import { FaSearch } from "react-icons/fa";
 
 function Header() {
-    return (
-      <header className="header">
-        <div className="header-left">
-          <RefreshButton />
-        </div>
+  return (
+    <header className="header">
+      <div className="header-left">
+        <RefreshButton />
+      </div>
 
-        <div className="header-center">
+      <div className="header-center">
+        <div className="search-wrapper">
           <input
             type="text"
             placeholder="Buscar en ManiaTico"
             className="search-bar"
           />
+          <button className="search-button">
+            <FaSearch className="search-icon"/>
+          </button>
         </div>
-        
-        <div className="header-right"></div>
-      </header>
-    );
+      </div>
+
+      <div className="header-right"></div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
