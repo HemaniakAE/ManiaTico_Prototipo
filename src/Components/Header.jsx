987ba6afd9c9f8ac88f49b3c6044a7bc1279e77b
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2025-11-19 11:54:43 -0600

    Traduccion de sidebar/header
    traducciones de Sidebar, header y Auth. completas

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 import RefreshButton from "./RefreshButton";
-import { Link } from "react-router-dom";
+import { FaBook, FaSearch, FaRegUserCircle } from "react-icons/fa";
 import SearchBar from "./Searchbar";
 import CartButton from "./CartButton";
-import { FaBook, FaRegUserCircle } from "react-icons/fa";
 import useTranslate from "../Context/useTranslate";
 
-export default function Header() {
+function Header() {
   const { t } = useTranslate();
 
   return (
     <header className="header">
+
       <div className="header-left">
         <RefreshButton />
       </div>
 
       <div className="header-center">
+
+        {/* BOTÓN BIBLIOTECA – REGRESADO A SU LUGAR ORIGINAL */}
+        <Link to="/library" className="library-link">
+          <FaBook className="library-icon" />
+          <span>{t("library")}</span>
+        </Link>
+
         <SearchBar />
       </div>
 
       <div className="header-right">
-        <Link to="/library" className="auth-button">
-          <FaBook className="library-icon" />
-          <span>{t("library")}</span>
-        </Link>
 
         <CartButton />
 
@@ -31,7 +36,11 @@ export default function Header() {
           <FaRegUserCircle className="auth-icon" />
           <span>{t("login")}</span>
         </Link>
+
       </div>
+
     </header>
   );
 }
+
+export default Header;
--- src/Context/useTranslate.js
+++ src/Context/useTranslate.js
@@ -2,16 +2,23 @@ import { useLanguage } from "./LanguageContext";
 import es from "../i18n/es.json";
 import en from "../i18n/en.json";
 
+const dictionaries = { es, en };
+
+// ⬇⬇⬇ Aquí se añade soporte para claves anidadas ⬇⬇⬇
+function getNestedValue(obj, path) {
+  return path.split(".").reduce((acc, key) => {
+    return acc && acc[key] !== undefined ? acc[key] : null;
+  }, obj);
+}
+
 export default function useTranslate() {
   const { language } = useLanguage();
-
-  const dictionary = {
-    es,
-    en,
-  };
+  const dict = dictionaries[language] || dictionaries["es"];
 
   function t(key) {
-    return dictionary[language]?.[key] || key;
+    if (!key) return "";
+    const value = getNestedValue(dict, key);
+    return value !== null ? value : key; // si no existe, retorna la key
   }
 
   return { t, language };
