e28387221774f62cdcd4a209fa0f237fa3b11c1d
YeremiCalvoPorras <calvoporrasyeremi@gmail.com>
2025-11-19 22:55:35 -0600

    TRAD/Library
    completa

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,27 +1,28 @@
-// src/Components/Catalog.jsx
 import React from "react";
 import gamesData from "../data/games.json";
 import GameCard from "./GameCard";
 import "./Catalog.css";
+import useTranslate from "../Context/useTranslate";
 
 const Catalog = () => {
+  const { t } = useTranslate();
   const featured = gamesData.find((g) => g.name === "Spirits of Irazú");
-  const popular = gamesData.filter((g) => g.id <= 5 && g.name !== featured.name);
+  const popular = gamesData.filter((g) => g.id <= 5 && g.name !== featured?.name);
   const rest = gamesData.filter((g) => g.id > 5);
 
   return (
     <div className="catalog-container">
-      <h2 className="catalog-title">Catálogo de Juegos</h2>
+      <h2 className="catalog-title">{t('catalogTitle')}</h2>
 
       {/* Featured */}
       <section className="featured-section">
-        <h3>Destacado</h3>
+        <h3>{t('featured')}</h3>
         <GameCard game={featured} variant="featured" />
       </section>
 
       {/* Populares */}
       <section className="popular-section">
-        <h3>Más Populares</h3>
+        <h3>{t('popular')}</h3>
         <div className="catalog-grid popular-grid">
           {popular.map((game) => (
             <GameCard key={game.id} game={game} />
@@ -31,7 +32,7 @@ const Catalog = () => {
 
       {/* Todos los juegos */}
       <section className="all-section">
-        <h3>Todos los juegos</h3>
+        <h3>{t('allGames')}</h3>
         <div className="catalog-grid">
           {rest.map((game) => (
             <GameCard key={game.id} game={game} />
@@ -43,8 +44,8 @@ const Catalog = () => {
       <section className="coming-soon-section">
         <div className="coming-soon-card">
           <div className="coming-soon-content">
-            <h3>¡Próximamente más juegos...!</h3>
-            <p>¡Se vienen nuevos títulos increíbles!</p>
+            <h3>{t('comingSoon')}</h3>
+            <p>{t('newTitles')}</p>
           </div>
         </div>
       </section>
